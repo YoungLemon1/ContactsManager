a4875d6b1bb4100e2574389572b38419fb63be83
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2023-03-08 20:10:36 +0200

    Add project files.

---
+++ HomeAssingment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HomeAssingment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
