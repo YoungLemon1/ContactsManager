eef71309a7a4b57d83f6ecbd0e672d8ef5145ab3
YoungLemon1 <110844957+YoungLemon1@users.noreply.github.com>
2023-03-10 20:51:43 +0200

    fixed typo, added null handling

--- HomeAssingment/Controllers/HomeController.cs
+++ HomeAssingment/Controllers/HomeController.cs
@@ -32,8 +32,14 @@ namespace HomeAssignment.Controllers
                     var contact = new Contact()
                     {
                         Id = reader.GetInt32(0),
-                        FirstName = reader.GetString(1),
-                        LastName = reader.GetString(2)
+                        FirstName = reader.IsDBNull(1) ? "" : reader.GetString(1),
+                        LastName = reader.IsDBNull(2) ? "" : reader.GetString(2),
+                        BirthDate = reader.IsDBNull(3) ? DateTime.MinValue : reader.GetDateTime(3),
+                        City = reader.IsDBNull(4) ? "" : reader.GetString(4),
+                        Street = reader.IsDBNull(5) ? "" : reader.GetString(5),
+                        HouseNumber = reader.IsDBNull(6) ? 0 : reader.GetInt32(6),
+                        PhoneAtHome = reader.IsDBNull(7) ? "" : reader.GetString(7),
+                        Phone = reader.IsDBNull(8) ? "" : reader.GetString(8)
                     };
 
                     contacts.Add(contact);
@@ -46,5 +52,23 @@ namespace HomeAssignment.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        //private string SetTableColumnInt(SqlDataReader ? reader, int column)
+        //{
+        //    if (reader.GetInt32(column) is null)
+        //        return "";
+        //    return reader.GetInt32(column).ToString();
+        //}
+        //private string SetTableColumnString(SqlDataReader ? reader, int column)
+        //{
+        //    if (reader.GetString(column) == null)
+        //        return "";
+        //    return reader.GetString(column).ToString();
+        //}
+
+        //private string SetTableColumnDateTime(SqlDataReader? reader, int column)
+        //{
+
+        //}
     }
 }
--- HomeAssingment/Models/Contact.cs
+++ HomeAssingment/Models/Contact.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateOnly ? BirthDate { get; set; }
+        public DateTime BirthDate { get; set; }
         public string ? City { get; set; }
         public string ? Street { get; set; }
         public int ? HouseNumber { get; set; }
