38b29e693bfddb9abbe991b95e9717361d30a3f9
yaniv <yaniv@DESKTOP-K7C29F9>
2023-03-16 19:53:17 +0200

    hifens, spaces and ' in first name and last name

--- HomeAssingment/Models/Contact.cs
+++ HomeAssingment/Models/Contact.cs
@@ -9,16 +9,17 @@ namespace HomeAssignment.Models
         [RegularExpression("[0-9]{9}", ErrorMessage = "ID should be 9 digits long")]
         public string Id { get; set; }
         [Required(ErrorMessage = "First Name is Required")]
-        [RegularExpression("[A-Za-z]{2,}", ErrorMessage = "First name should be at least 2 letters long")]
+        [RegularExpression("[A-Za-z\\s\\-\\']{2,}", ErrorMessage = "First name should be at least 2 letters long")]
         public string FirstName { get; set; }
         [Required(ErrorMessage = "Last Name is Required")]
-        [RegularExpression("[A-Za-z]{2,}", ErrorMessage = "Last name should be at least 2 letters long")]
+        [RegularExpression("[A-Za-z\\s\\-\\']{2,}", ErrorMessage = "Last name should be at least 2 letters long")]
         public string LastName { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Range(typeof(DateTime), "1/1/1900", "1/1/2023", ErrorMessage = "Date out of range")]
         public DateTime? BirthDate { get; set; }
-        [RegularExpression("[A-Za-z\\s\\-]{2,}", ErrorMessage = "City should be at least 2 letters long")]
+        [RegularExpression("[A-Za-z\\s\\-\\']{2,}", ErrorMessage = "City should be at least 2 letters long")]
         public string? City { get; set; }
-        [RegularExpression("[A-Za-z\\s\\-]{2,}", ErrorMessage = "Street should be at least 2 letters long")]
+        [RegularExpression("[A-Za-z\\s\\-\\']{2,}", ErrorMessage = "Street should be at least 2 letters long")]
         public string? Street { get; set; }
         [RegularExpression("[0-9]+", ErrorMessage = "House number should be 4 digits long")]
         public int? HouseNumber { get; set; }
